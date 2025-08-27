6ccd2734ac38ac91bfaef4dfabfff92735023f32
yuto ichinose <yuto.0526.sano@gmail.com>
2025-08-28 00:27:37 +0900

    ci:  vrt導入

---
+++ .storybook/test-runner.js
@@ -0,0 +1,22 @@
+/* global expect */
+// Storybook Test Runner 設定例
+import { toMatchImageSnapshot } from "jest-image-snapshot";
+
+const VIEWPORT = { width: 1280, height: 800 };
+
+export default {
+  setup() {
+    expect.extend({ toMatchImageSnapshot });
+  },
+  async preVisit(page) {
+    // 画面幅を明示的に指定
+    await page.setViewportSize(VIEWPORT);
+  },
+  async postVisit(page, context) {
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      customSnapshotsDir: "__image_snapshots__",
+      customSnapshotIdentifier: context.id,
+    });
+  },
+};
